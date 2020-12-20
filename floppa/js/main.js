c3d2d7c313ec46c68fbf61b1638c17f531459315
SenorNoName <SenorNoName@users.noreply.github.com>
2020-12-19 19:08:00 -0700

    commit

--- floppa/js/main.js
+++ floppa/js/main.js
@@ -1,3 +1,16 @@
-for (i = 1; i < 106; i++) {
-  //lol
+function dispImg(src, int) {
+  var img = document.createElement("img");
+  img.src = src;
+  img.classList.add("gallery_img");
+
+  var figure = document.createElement("figure");
+  figure.id = "item" + int;
+  figure.classList.add("gallery__item");
+
+  document.getElementById("gal").appendChild(figure);
+  document.getElementById("item" + int).appendChild(img);
+}
+
+for (i = 1; i < 107; i++) {
+  dispImg("images/floppa" + i + ".jpg", i);
 }
