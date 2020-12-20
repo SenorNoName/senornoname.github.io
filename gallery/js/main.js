5759fe7a4ebf6e87ab0608732bfed9641d9e6e77
SenorNoName <SenorNoName@users.noreply.github.com>
2020-12-20 09:26:36 -0700

    commit

--- gallery/js/main.js
+++ gallery/js/main.js
@@ -2,10 +2,11 @@ function dispImg(src, int) {
   var img = document.createElement("img");
   img.src = src;
   img.classList.add("gallery_img");
+  img.classList.add("zoom");
 
   var figure = document.createElement("figure");
   figure.id = "item" + int;
-  figure.classList.add("gallery__item");
+  figure.classList.add("gallery_item");
 
   document.getElementById("gal").appendChild(figure);
   document.getElementById("item" + int).appendChild(img);
