e5163dde2845daddeaa0618dce85512510f79229
SenorNoName <SenorNoName@users.noreply.github.com>
2021-01-10 18:46:57 -0700

    Update main.js

--- chat/main.js
+++ chat/main.js
@@ -1,59 +1,61 @@
-const Html5WebSocket = require('html5-websocket');
-const ReconnectingWebSocket = require('reconnecting-websocket');
+// initialize websockets:
+var ws_uri = "wss:bigfloppa.net/chat:9600";
+var websocket = new WebSocket(ws_uri);
 
-let ws_host = 'bigfloppa.net/chat';
-let ws_port = '9600';
-const options = { WebSocket: Html5WebSocket };
-const rws = new ReconnectingWebSocket('wss://' + ws_host + ':' + ws_port + '/ws', undefined, options);
-rws.timeout = 1000;
 
-// on open
-rws.addEventListener('open', () => {
-  MessageAdd('<div class="message green">You have entered the chat room.</div>');
-});
-
-// on close
-rws.addEventListener('close', () => {
-  MessageAdd('<div class="message blue">You have been disconnected.</div>');
-});
-
-// on error
-rws.onerror = (err) => {
-  MessageAdd('<div class="message red">Connection to chat failed.</div>');
-  if (err.code == 'EHOSTDOWN') {
-        console.log('[Client] Error: server down.');
-  }
+// on websocket open:
+websocket.onopen = function(event) {
+    MessageAdd('<div class="message green">You have entered the chat room.</div>');
 };
 
-rws.addEventListener('message', (e) => {
-  var data = JSON.parse(event.data);
 
-  if (data.type == "message") {
-    MessageAdd('<div class="message">' + data.username + ': ' + data.message + '</div>');
-  }
-});
+// on websocket close:
+websocket.onclose = function(event) {
+    MessageAdd('<div class="message blue">You have been disconnected.</div>');
+};
 
+
+// on websocket error:
+websocket.onerror = function(event) {
+    MessageAdd('<div class="message red">Connection to chat failed.</div>');
+};
+
+
+// on websocket message received:
+websocket.onmessage = function(event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type == "message") {
+        MessageAdd('<div class="message">' + data.username + ': ' + data.message + '</div>');
+    }
+};
+
+
+// on chat form submit:
 document.getElementById("chat-form").addEventListener("submit", function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  var message_element = document.getElementsByTagName("input")[0];
-  var message = message_element.value;
+    var message_element = document.getElementsByTagName("input")[0];
+    var message = message_element.value;
 
-  if (message.toString().length) {
-    var data = {
-      type: "message",
-      username: "You",
-      message: message
-    };
+    if (message.toString().length) {
+        var data = {
+            type: "message",
+            username: "You",
+            message: message
+        };
 
-    websocket.send(JSON.stringify(data));
-    message_element.value = "";
-  }
+        websocket.send(JSON.stringify(data));
+
+        message_element.value = "";
+    }
 }, false);
 
-function MessageAdd(message) {
-  var chat_messages = document.getElementById("chat-messages");
 
-  chat_messages.insertAdjacentHTML("beforeend", message);
-  chat_messages.scrollTop = chat_messages.scrollHeight;
+// add message to chat:
+function MessageAdd(message) {
+    var chat_messages = document.getElementById("chat-messages");
+
+    chat_messages.insertAdjacentHTML("beforeend", message);
+    chat_messages.scrollTop = chat_messages.scrollHeight;
 }
