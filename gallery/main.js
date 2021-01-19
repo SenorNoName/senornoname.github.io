642803d879d85b38a1020c0b896671007166bed7
SenorNoName <SenorNoName@users.noreply.github.com>
2021-01-19 00:41:51 -0700

    Update main.js

--- gallery/main.js
+++ gallery/main.js
@@ -1,7 +1,5 @@
-var img;
-
 function dispImg(src, int) {
-  img = document.createElement("img");
+  var img = document.createElement("img");
   img.src = src;
   img.classList.add("gallery_img");
   img.classList.add("zoom");
@@ -14,14 +12,6 @@ function dispImg(src, int) {
   document.getElementById("item" + int).appendChild(img);
 }
 
-/*for (i = 1; i < 107; i++) {
+for (i = 1; i < 107; i++) {
   dispImg("images/floppa" + i + ".jpg", i);
-}*/
-
-var i = 1;
-while (i < 107) {
-  dispImg("images/floppa" + i + ".jpg", i);
-  img.onload = function() {
-    i++;
-  };
 }
