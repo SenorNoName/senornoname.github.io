115ce67b2b72b5afb5e560a03a16afe02b557e20
SenorNoName <SenorNoName@users.noreply.github.com>
2021-01-19 00:28:37 -0700

    Update main.js

--- gallery/main.js
+++ gallery/main.js
@@ -14,14 +14,14 @@ function dispImg(src, int) {
   document.getElementById("item" + int).appendChild(img);
 }
 
-/*for (i = 1; i < 107; i++) {
+for (i = 1; i < 107; i++) {
   dispImg("images/floppa" + i + ".jpg", i);
-}*/
+}
 
-var i = 1;
+/*var i = 1;
 while (i < 107) {
   dispImg("images/floppa" + i + ".jpg", i);
   img.onload = function() {
     i++;
   };
-}
+}*/
