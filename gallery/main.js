275cd95d51aa0de9c3af025f3bf4aa33300de3b0
SenorNoName <SenorNoName@users.noreply.github.com>
2021-01-19 00:23:00 -0700

    commit

--- gallery/main.js
+++ gallery/main.js
@@ -1,5 +1,7 @@
+var img;
+
 function dispImg(src, int) {
-  var img = document.createElement("img");
+  img = document.createElement("img");
   img.src = src;
   img.classList.add("gallery_img");
   img.classList.add("zoom");
@@ -12,6 +14,14 @@ function dispImg(src, int) {
   document.getElementById("item" + int).appendChild(img);
 }
 
-for (i = 1; i < 107; i++) {
+/*for (i = 1; i < 107; i++) {
   dispImg("images/floppa" + i + ".jpg", i);
+}*/
+
+var i = 1;
+while (i < 107) {
+  dispImg("images/floppa" + i + ".jpg", i);
+  img.onload = function() {
+    i++;
+  }
 }
