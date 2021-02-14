f5777936a68d692451c0701ffbd7dececd22351c
SenorNoName <SenorNoName@users.noreply.github.com>
2021-02-13 21:05:26 -0700

    commit

--- gallery/main.js
+++ gallery/main.js
@@ -1,10 +1,12 @@
-var i = 1;
+var index;
+var flag = false;
 
-function dispImg(src, int, _callback) {
+function dispImg(src, int) {
   var img = document.createElement("img");
   img.src = src;
   img.classList.add("gallery_img");
   img.classList.add("zoom");
+  img.id = "img" + int;
 
   var figure = document.createElement("figure");
   figure.id = "item" + int;
@@ -12,18 +14,54 @@ function dispImg(src, int, _callback) {
 
   document.getElementById("gal").appendChild(figure);
   document.getElementById("item" + int).appendChild(img);
-  img.onload = _callback();
 }
 
-while (i < 107) {
-  dispImg("images/floppa" + i + ".jpg", i, function() {
-    i++;
-  });
+for (i = 1; i < 26; i++) {
+  dispImg("images/floppa" + i + ".jpg", i);
+  index = 26;
 }
 
-i = 109;
-while (i < 205) {
-  dispImg("images/floppa" + i + ".jpg", i, function() {
-    i++;
-  });
+function back() {
+  if (flag == true) {
+    for (i = 5; i < 26; i++) {
+      var image = document.createElement("img");
+      image.classList.add("gallery_img");
+      image.classList.add("zoom");
+      image.id = "img" + i;
+      document.getElementById("item" + i).appendChild(image);
+    }
+    flag = false;
+  }
+  if (index != 26) {
+    index -= 50;
+    for (i = 1; i < 26; i++) {
+      var newImg = document.getElementById("img" + i);
+      newImg.src = "images/floppa" + index + ".jpg";
+      document.getElementById("item" + i).appendChild(newImg);
+      index++;
+    }
+  }
+}
+
+function forward() {
+  if (index == 201) {
+    for (i = 1; i < 5; i++) {
+      var newImg = document.getElementById("img" + i);
+      newImg.src = "images/floppa" + index + ".jpg";
+      document.getElementById("item" + i).appendChild(newImg);
+      index++;
+    }
+    for (i = 5; i < 26; i++) {
+      document.getElementById("img" + i).remove();
+    }
+    flag = true;
+    index = 226;
+  } else {
+    for (i = 1; i < 26; i++) {
+      var newImg = document.getElementById("img" + i);
+      newImg.src = "images/floppa" + index + ".jpg";
+      document.getElementById("item" + i).appendChild(newImg);
+      index++;
+    }
+  }
 }
