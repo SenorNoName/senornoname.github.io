3232a00eb1ad9a740531206c72ac05286ec60620
SenorNoName <ibj6903@gmail.com>
2022-07-05 18:08:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,51 +5,51 @@ var text = document.getElementById("time");
 switch (d.getMonth()) {
   case 0:
     img.src = "images/flop_jan_2021.jpg";
-    text.textContent += "January 2021";
+    text.textContent += "January " + (d.getYear()+1900);
     break;
   case 1:
     img.src = "images/flop_feb_2021.png";
-    text.textContent += "February 2021"
+    text.textContent += "February " + (d.getYear()+1900);
     break;
   case 2:
     img.src = "images/flop_mar_2021.jpg";
-    text.textContent += "March 2021"
+    text.textContent += "March " + (d.getYear()+1900);
     break;
   case 3:
     img.src = "images/flop_apr_2021.jpg";
-    text.textContent += "April 2021"
+    text.textContent += "April " + (d.getYear()+1900);
     break;
   case 4:
     img.src = "images/flop_may_2021.png";
-    text.textContent += "May 2021"
+    text.textContent += "May " + (d.getYear()+1900);
     break;
   case 5:
     img.src = "images/flop_jun_2021.png";
-    text.textContent += "June 2021"
+    text.textContent += "June " + (d.getYear()+1900);
     break;
   case 6:
     img.src = "images/flop_jul_2021.png";
-    text.textContent += "July 2021"
+    text.textContent += "July " + (d.getYear()+1900);
     break;
   case 7:
     img.src = "images/flop_aug_2021.png";
-    text.textContent += "August 2021"
+    text.textContent += "August " + (d.getYear()+1900);
     break;
   case 8:
     img.src = "images/flop_sep_2021.png";
-    text.textContent += "September 2021"
+    text.textContent += "September " + (d.getYear()+1900);
     break;
   case 9:
     img.src = "images/flop_oct_2021.png";
-    text.textContent += "October 2021"
+    text.textContent += "October " + (d.getYear()+1900);
     break;
   case 10:
     img.src = "images/flop_nov_2021.png";
-    text.textContent += "November 2021"
+    text.textContent += "November " + (d.getYear()+1900);
     break;
   case 11:
     img.src = "images/flop_dec_2021.png";
-    text.textContent += "December 2021"
+    text.textContent += "December " + (d.getYear()+1900);
     break;
 }
 
